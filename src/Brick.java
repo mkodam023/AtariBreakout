2f11dbf47ac683f898e92b9ba5e78b1ecf6fd79f
mkodam023 <mkodam023@student.fuhsd.org>
2023-12-13 14:59:09 -0800

    Added initial code

--- src/Brick.java
+++ src/Brick.java
@@ -7,16 +7,7 @@ public class Brick {
     boolean ifHit;
 
 
-
-    public int getxSize() {
-        return xSize;
-    }
-
-    public int getySize() {
-        return ySize;
-    }
-
-    public Brick(int x, int y, int xs, int ys){
+    public Brick(int x, int y, int xs, int ys) {
         this.x = x;
         this.y = y;
         ifHit = false;
@@ -24,44 +15,54 @@ public class Brick {
         this.ySize = ys;
     }
 
-    public int getX() {
-        return x;
-    }
-    public int getY() {
-        return y;
-    }
 
     public void setX(int x) {
         this.x = x;
     }
-
-    public void ifHit(PApplet window){
-        this.x = 800;
-
+    public void setY(int y) {
+        this.y = y;
     }
 
-    public void draw(PApplet window){
-        if (y<20){
-            window.fill(255,0,0);       // red
-          //  y=y;
-        }else if(y>39&&y<=80){
-            window.fill(255,165,0);     // orange
-           // y= y+20;
-        }else if(y>79&&y<=120){
-            window.fill(255,255,10);   // yellow
-          //  y=y+40;
-        }else if(y>119&&y<=160){
-            window.fill(0,255,0);      // green
-          //  y=y+60;
-        }else if(y>159&&y<=200){
-            window.fill(0,0,255);      //blue
-           // y=y+80;
-        }else if(y>199&&y<=240){
-            window.fill(255,0,255);    //purple
-           // y = y+100;
+
+    public void ifHit(PApplet window) {
+        this.x = 80000;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void draw(PApplet window) {
+        if (y < 40) {
+            window.fill(255, 0, 0);       // red
+
+        } else if (y >= 40 && y < 80) {
+            window.fill(255, 165, 0);     // orange
+
+        } else if (y >= 80 && y < 120) {
+            window.fill(255, 255, 10);   // yellow
+
+        } else if (y >= 120 && y < 140) {
+            window.fill(0, 255, 0);      // green
+
+        } else if (y >= 140 && y < 180) {
+            window.fill(0, 0, 255);      //blue
+
+        } else if (y >= 180 && y < 240) {
+            window.fill(255, 0, 255);    //purple
         }
         window.rect(x,y,xSize,ySize);
+    }
 
 
     }
-}
+
+
+
+
+
+
