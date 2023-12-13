2be258199e4e60df982649c921d93f0cf99474ac
mkodam023 <mkodam023@student.fuhsd.org>
2023-12-13 15:00:25 -0800

    Added initial code

--- src/Paddle.java
+++ src/Paddle.java
@@ -1,9 +1,16 @@
 import processing.core.PApplet;
 
 public class Paddle {
-        int x,y;
-        int xSize;
-        int ySize;
+    int x, y;
+    int xSize;
+    int ySize;
+
+    public Paddle(int x, int y, int xs, int ys) {
+        this.x = x;
+        this.y = y;
+        this.xSize = xs;
+        this.ySize = ys;
+    }
 
     public int getY() {
         return y;
@@ -13,31 +20,18 @@ public class Paddle {
         this.y = y;
     }
 
-    public Paddle(int x, int y, int xs, int ys){
-            this.x = x;
-            this.y = y;
-            this.xSize = xs;
-            this.ySize = ys;
-        }
-
-        public void ballHit(Ball ball){
-            //if the ball hits the paddle change ball direction
-
-        }
-
-        public void draw(PApplet window){
-            window.fill(255,255,255);
-            window.rect(x, 600, xSize, ySize);
-        }
-
-        public void move(PApplet window){
-            x = window.mouseX-50;
-        }
-
-
-
-
-
-
+    public void ballHit(Ball ball) {
+        //if the ball hits the paddle change ball direction
 
     }
+
+    public void draw(PApplet window) {
+        window.fill(255, 255, 255);
+        window.rect(x, 600, xSize, ySize);
+    }
+
+    public void move(PApplet window) {
+        x = window.mouseX - 50;
+    }
+
+}
